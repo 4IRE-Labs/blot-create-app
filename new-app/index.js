e0b85989d5e8c2d923b6d53d31a3630c7abf2f3a
AKO <a.kovtun@4irelabs.com>
2021-09-07 19:23:53 +0300

    wip

--- new-app/index.js
+++ new-app/index.js
@@ -1,1 +1,42 @@
-console.log('hello world')
+// import  {Blot, NetworkProvider, Account} from 'blot';
+const state = {
+    blot: null,
+    provider: null,
+    allAccounts: null,
+    value: 0
+}
+// const initProvider = async () => {
+//     state.provider = await new NetworkProvider('ws://127.0.0.1:9944').connect();
+// }
+
+// const initBlot = async () => {
+//     const blot = new Blot(provider);
+//     state.blot = blot
+//     try {
+//         await blot.enable('Example Vanilla App');
+//       } catch (err) {
+//         console.warn("User didn't accept authorization or no extension installed.")
+//     }
+//     state.allAccounts = await blot.getAccounts();
+// }
+
+window.setValue = (value) => {
+    state.value = value;
+    document.getElementById("countValue").innerHTML = value;
+}
+window.getValue = async () => {
+    console.log('getValue')
+    const getValueFromContract = 0;
+    window.setValue(0)
+}
+
+window.increment = async () => {
+    console.log('incriment');
+    window.setValue(state.value + 1)
+}
+
+window.decrement = async () => {
+    console.log('decrement');
+    window.setValue(state.value - 1)
+}
+
